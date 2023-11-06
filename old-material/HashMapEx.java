e523e59985e68b27f4aa01d02f32df991371bcd4
sarahcarroll7 <149721303+sarahcarroll7@users.noreply.github.com>
2023-11-06 11:50:14 -0500

    S
    Sarah Carroll

--- old-material/HashMapEx.java
+++ old-material/HashMapEx.java
@@ -1,34 +1,29 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class HashMapEx {
-	
-	public static void main(String[] args) {
-		Map vehicles = new HashMap();
-		
-		// Add some vehicles.
-		vehicles.put("BMW", 5);
-		vehicles.put("Mercedes", 3);
-		vehicles.put("Audi", 4);
-		vehicles.put("Ford", 10);
-		
-		System.out.println("Total vehicles: " + vehicles.size());
-		
-		// Iterate over all vehicles, using the keySet method.
-		for(String key: vehicles.keySet())
-			System.out.println(key + " - " + vehicles.get(key));
-		System.out.println();
+class hashmapex{
+
+	public static void main(String args[]){
+
+		Map vehicles = new HashMap(); 
+		vehicles.put("Maruti", 2); 
+		vehicles.put("Benz", 4);
+		vehicles.put("Mahindra", 5);
+
+		System.out.print("\t" + vehicles);
+		//for(String key: vehicles.keySet()){
 		
 		String searchKey = "Audi";
 		if(vehicles.containsKey(searchKey))
 			System.out.println("Found total " + vehicles.get(searchKey) + " "
 					+ searchKey + " cars!\n");
 		
-		// Clear all values.
-		vehicles.clear();
-		
-		// Equals to zero.
-		System.out.println("After clear operation, size: " + vehicles.size()); 
-	}
-}
+// 		String searchkey = "Maruti";
 
+//		if(vehicles.containsKey(searchkey));
+//		System.out.println("Total " + vehicles.get(searchkey) + " vehicles " + searchkey + " found");
+
+		
+	}
+//System.out.println();
+}
